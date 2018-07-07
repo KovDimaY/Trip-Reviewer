089a74dd7943bfec170e0008f2e45ec5676c9d40
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-07-07 23:45:22 +0200

    Started work with the client, set up react environment

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from './reducers'; 
+import Routes from './routes';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
---
+++ client/src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+
+const Routes = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default Routes;
