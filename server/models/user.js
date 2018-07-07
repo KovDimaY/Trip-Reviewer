1bb4ac37ec7b96ab6c3a946962be5950b914a99d
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-07-07 20:36:05 +0200

    Implemented register, login and reviewers methods in backend

--- server/models/user.js
+++ server/models/user.js
@@ -34,6 +34,45 @@ const userSchema = mongoose.Schema({
     }
 });
 
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    if(user.isModified('password')) {
+        bcrypt.genSalt(SALT_I, function(err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+    const user = this;
+
+    bcrypt.compare(candidatePassword, user.password, function(err, isMatch) {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
+};
+
+userSchema.methods.generateToken = function(callback) {
+    const user = this;
+    const token = jwt.sign(user._id.toHexString(), config.SECRET);
+
+    user.token = token;
+    user.save(function(err, user) {
+        if (err) return callback(err);
+        callback(null, user)
+    });
+};
+
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ app.get('/api/getTrip', (req,res) => {
 
     Trip.findById(id, (err, doc) => {
         if (err) return res.status(400).send(err);
+        
         res.send(doc);
     });
 });
@@ -40,8 +41,32 @@ app.get('/api/getManyTrips', (req,res) => {
     // ORDER = asc || desc
     Trip.find().skip(skip).sort({_id: order}).limit(limit).exec((err, doc) => {
         if (err) return res.status(400).send(err);
+
         res.send(doc);
     })
+});
+
+// locahost:3001/api/getReviewer?id=5b40f519c1632805acd50639
+app.get('/api/getReviewer', (req, res) => {
+    const id = req.query.id;
+
+    User.findById(id, (err, doc) => {
+        if (err) return res.status(400).send(err);
+
+        res.json({
+            name: doc.name,
+            lastname: doc.lastname
+        });
+    });
+});
+
+
+app.get('/api/users', (req, res) => {
+    User.find({}, (err, users) => {
+        if (err) return res.status(400).send(err);
+
+        res.status(200).send(users);
+    });
 });
 
 
@@ -55,6 +80,42 @@ app.post('/api/trip', (req, res) => {
         res.status(200).json({
             post: true,
             tripId: doc._id
+        });
+    });
+});
+
+app.post('/api/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false });
+        
+        res.status(200).json({
+            success: true,
+            user:doc
+        });
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    User.findOne({ 'email': req.body.email }, (err, user) => {
+        if (!user) return res.json({ isAuth: false, message: 'Auth failed, email not found'});
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({
+                isAuth: false,
+                message: 'Wrong password'
+            });
+
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                res.cookie('auth', user.token).json({
+                    isAuth: true,
+                    id: user._id,
+                    email: user.email
+                });
+            });
         });
     });
 });
