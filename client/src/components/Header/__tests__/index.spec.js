8c01f17417df11b3228ebd41e5e87111bcea9016
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-07-30 22:34:00 +0200

    DEV-4 all the components covered by test 100%

--- client/src/components/Header/__tests__/index.spec.js
+++ client/src/components/Header/__tests__/index.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
+import { mount } from 'enzyme';
  
 import Header from './../../Header';
 
@@ -17,5 +18,21 @@ describe('<Header />', () => {
         const tree = create(mockComponent()).toJSON();
         
         expect(tree).toMatchSnapshot();
+    });
+
+    it('should change showNav state to false when handleHideNav is triggered', () => {
+        const instance = mount(mockComponent()).instance();
+    
+        instance.handleHideNav();
+    
+        expect(instance.state.showNav).toEqual(false);
+    });
+
+    it('should change showNav state to true when handleOpenNav is triggered', () => {
+        const instance = mount(mockComponent()).instance();
+    
+        instance.handleOpenNav();
+    
+        expect(instance.state.showNav).toEqual(true);
     });
 });
--- client/src/components/Logout/__tests__/index.spec.js
+++ client/src/components/Logout/__tests__/index.spec.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
+import { mount } from 'enzyme';
+import moxios from 'moxios';
  
 import Logout from './../../Logout';
 
@@ -8,11 +10,32 @@ const mockComponent = props => {
         <Logout {...props} />
     );
 };
- 
+
 describe('<Logout />', () => {
     it('should render component', () => {
         const tree = create(mockComponent()).toJSON();
         
         expect(tree).toMatchSnapshot();
+    });
+
+    it('should redirect to home after some time', (done) => {
+        moxios.install();
+        moxios.stubRequest('/api/logout', {
+            status: 200,
+            response: {}
+        });
+
+        const props = {
+            history: {
+                push: jest.fn()
+            }
+        };
+        const instance = mount(mockComponent(props));
+
+        setTimeout(() => {
+            expect(props.history.push).toHaveBeenCalledWith('/');
+            moxios.uninstall();
+            done();
+        }, 3000);
     });
 });
