7073d26e1956a496e5f8cbe6282a717bf6a99b53
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-08-08 13:25:19 +0200

    DEV-5 achieved 100% coverage for the first container

--- client/src/containers/AddTrip/__tests__/index.spec.js
+++ client/src/containers/AddTrip/__tests__/index.spec.js
@@ -1,28 +1,45 @@
 import React from 'react';
+import configureStore from 'redux-mock-store';
 import { create } from 'react-test-renderer';
 import { shallow } from 'enzyme';
  
 import AddTrip from './../../AddTrip';
-import Root from './../../../hoc/root';
 
-const mockComponent = props => {
+const mockStore = configureStore();
+
+const mockComponent = (initialState = {}, props) => {
+    const store = mockStore(initialState);
+
     return (
-        <AddTrip {...props} />
+        <AddTrip {...props} store={store} />
     );
 };
  
 describe('<AddTrip />', () => {
     it('should render component', () => {
-        const tree = create(mockComponent()).toJSON();
+        const initialState = {
+            trips: {}
+        };
+        const tree = create(mockComponent(initialState)).toJSON();
         
         expect(tree).toMatchSnapshot();
     });
 
     it('handleInput should change state correctly', () => {
-        const instance = shallow(mockComponent()).instance();
+        const initialState = {
+            trips: {}
+        };
+        const event = {
+            target: {
+                value: 'test',
+                name: 'title'
+            }
+        };
+
+        const instance = shallow(mockComponent(initialState)).dive().instance();
     
-        instance.handleInput();
+        instance.handleInput(event);
     
-        expect(instance.state.showNav).toEqual(false);
+        expect(instance.state.formdata.title).toEqual('test');
     });
 });
