7cd410eabac6ac870428938b33fbbfab9ce85ab2
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-09-13 12:53:44 +0200

    DEV-22 fixed comments of the PR

--- client/src/components/User/index.js
+++ client/src/components/User/index.js
@@ -1,29 +1,27 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import UserAvatar from './../UserAvatar';
 
 import './styles.css';
 
-class User extends PureComponent {
-    render() {
-        const user = this.props.users.login;
-        const redirect = `/user/edit-profile/${user.id}`;
+const User = (props) => {
+    const user = props.users.login;
+    const redirect = `/user/edit-profile/${user.id}`;
 
-        return (
-            <div className="user_container">
-                <UserAvatar filename={user.avatar} />
-                <div className="data">
-                    <div><span>Name:</span> {user.name}</div>
-                    <div><span>Lastname:</span> {user.lastname}</div>
-                    <div><span>Email:</span> {user.email}</div>
-                </div>
-                <div className="text-center">
-                    <Link to={redirect} className="button-link">Edit Profile</Link>
-                </div>
+    return (
+        <div className="user_container">
+            <UserAvatar filename={user.avatar} />
+            <div className="data">
+                <div><span>Name:</span> {user.name}</div>
+                <div><span>Lastname:</span> {user.lastname}</div>
+                <div><span>Email:</span> {user.email}</div>
             </div>
-        );
-    }
+            <div className="text-center">
+                <Link to={redirect} className="button-link">Edit Profile</Link>
+            </div>
+        </div>
+    );
 };
 
 export default User;
