d0f58c5eaa7e92c06e715625f65dce81456444cd
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-10-03 00:52:42 +0200

    DEV-35 implemented 1h cache for the image of avatar

--- client/src/components/ImageUploader/index.js
+++ client/src/components/ImageUploader/index.js
@@ -41,6 +41,7 @@ class ImageUploader extends Component {
         accept="image/*"
         name="avatar"
         filename={this.props.filename}
+        metadata={{ cacheControl: 'max-age=3600' }}
         storageRef={firebase.storage().ref('avatars')}
         onUploadStart={this.handleUploadStart}
         onUploadError={this.handleUploadError}
